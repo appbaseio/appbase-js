ae26be14fbbf05136b33774dc65b02f7a2541f13
Siddharth Kothari <sids.aquarius@gmail.com>
2016-06-24 10:55:55 -0500

    more sensible test for searchStream single document

--- test/stream_search_test.js
+++ test/stream_search_test.js
@@ -7,58 +7,8 @@ streamSearchTests.streamMatchAllSingleType = function streamMatchAll(streamingCl
 		"user": "olivere",
 		"message": "Welcome to Golang and Elasticsearch."
 	}
-	streamingClient.index({
-		type: 'tweet',
-		id: '1',
-		body: tweet
-	}).on('data', function(res) {
-		var first = true
-		var responseStream = streamingClient.searchStream({
-			type: 'tweet',
-			body: {
-				query: {
-					match_all: {}
-				}
-			}
-		})
-		responseStream.on('error', function(err) {
-			if (err) {
-				done(err)
-				return
-			}
-		})
-		responseStream.on('data', function(res) {
-			if (first) {
-				setTimeout(function() {
-					streamingClient.index({
-						type: 'tweet',
-						id: '1',
-						body: tweet
-					}).on('error', function(err) {
-						if (err) {
-							done(err)
-							return
-						}
-					})
-				}, 2000)
-				first = false
-			} else {
-				try {
-					assert.deepEqual(res, {
-						_type: 'tweet',
-						_id: '1',
-						_source: tweet
-					}, 'event not as expected')
-				} catch (e) {
-					responseStream.stop()
-					return done(e)
-				}
-
-				responseStream.stop()
-				done()
-			}
-		})
-
+	var first = true
+	var indexFunc = function() {
 		streamingClient.index({
 			type: 'tweet',
 			id: '1',
@@ -69,6 +19,34 @@ streamSearchTests.streamMatchAllSingleType = function streamMatchAll(streamingCl
 				return
 			}
 		})
+	}
+	var responseStream = streamingClient.searchStream({
+		type: 'tweet',
+		body: {
+			query: {
+				match_all: {}
+			}
+		}
+	})
+	setTimeout(indexFunc, 100)
+	responseStream.on('data', function(res) {
+		if (first) {
+			indexFunc()
+			first = false
+		} else {
+			try {
+				assert.deepEqual(res, {
+					_type: 'tweet',
+					_id: '1',
+					_source: tweet
+				}, 'event not as expected')
+			} catch (e) {
+				responseStream.stop()
+				return done(e)
+			}
+			responseStream.stop()
+			done()
+		}
 	}).on('error', function(err) {
 		if (err) {
 			done(err)
