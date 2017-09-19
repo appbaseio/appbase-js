2ca546a4334b7fcb827b8556c2f11969a311ae3b
Deepak Grover <deepakgrover333@gmail.com>
2017-09-20 05:01:11 +0530

    cleanup and build

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,6 @@ module.exports = function(grunt) {
 			}
 		}
 	})
-
-	grunt.loadNpmTasks('grunt-babel')
-	grunt.loadNpmTasks('grunt-contrib-watch')
-	grunt.loadNpmTasks('grunt-browserify')
-	grunt.loadNpmTasks('grunt-contrib-uglify')
 	grunt.loadNpmTasks('grunt-mocha-test')
 
 	grunt.registerTask('test', [ 'mochaTest' ])
