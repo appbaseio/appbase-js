668993e3233f4fbfc150ed403ae1c0d117d9e23d
kuldeep <kuldeep@MacBook-Air.local>
2018-08-16 19:18:41 +0530

    Initial Setup

--- rollup.config.js
+++ rollup.config.js
@@ -3,32 +3,25 @@ import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/main.js',
-		output: {
-			name: 'howLongUntilLunch',
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
-		]
-	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
-	// `file` and `format` for each target)
-	{
-		input: 'src/main.js',
-		external: ['ms'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
-	}
+  // browser-friendly UMD build
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'appbase-js',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    plugins: [resolve(), commonjs()],
+  },
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.js',
+    external: ['ms'],
+    output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
+  },
 ];
