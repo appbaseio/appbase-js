2ec7c562199342d31578d4ca3bf6dd29685e5893
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-08-24 16:40:24 +0530

    feat: add util method to validate url

--- src/utils/index.js
+++ src/utils/index.js
@@ -231,3 +231,15 @@ export function validateSchema(
     }
   });
 }
+
+export function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
