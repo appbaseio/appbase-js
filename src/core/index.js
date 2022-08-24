dd1e1434917e37420b28838b70da2732536ad24f
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-08-24 16:40:40 +0530

    modify schema based on endpoint property

--- src/core/index.js
+++ src/core/index.js
@@ -1,5 +1,10 @@
 import URL from 'url-parser-lite';
-import { backendAlias, isAppbase, validateSchema } from '../utils/index';
+import {
+  backendAlias,
+  isAppbase,
+  isValidHttpUrl,
+  validateSchema,
+} from '../utils/index';
 import SCHEMA from '../utils/schema/index';
 /**
  * Returns an instance of Appbase client
@@ -11,6 +16,7 @@ import SCHEMA from '../utils/schema/index';
  * @param {String} config.password
  * @param {Boolean} config.enableTelemetry
  * @param {Object} config.mongodb
+ * @param {Object} config.endpoint
  * A callback function which will be invoked before a fetch request made
  */
 function AppBase(config) {
@@ -29,6 +35,13 @@ function AppBase(config) {
   const backendName = backendAlias[config.mongodb ? 'MONGODB' : 'ELASTICSEARCH'];
   // eslint-disable-next-line
   const schema = SCHEMA[backendName];
+
+  if (config.endpoint && isValidHttpUrl(config.endpoint.url)) {
+    schema.url.required = false;
+    schema.url.app = false;
+    schema.url.credentials = false;
+  }
+
   validateSchema(
     {
       url: config.url,
